f5edbdcf77f676b0f207f283f54f304ccd38a5cb
guilherme <guilhermepfm@gmail.com>
2025-06-13 13:00:36 -0300

    [feat] add module for training dataset

---
+++ Classifier/training.py
@@ -0,0 +1,20 @@
+from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV
+from sklearn.model_selection import StratifiedKFold
+import pandas as pd
+from typing import Tuple
+from feature_engineering import TARGET_VARIABLE
+
+
+def split_train_test(complete_dataset:pd.DataFrame, test_size:float) -> Tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
+    """
+    Splits the complete dataset into training and testing sets.
+    Args:
+        complete_dataset (pd.DataFrame): The complete dataset containing features and target variable.
+    Returns:
+        tuple: A tuple containing the training features (X_train), testing features (X_test),
+               training labels (y_train), and testing labels (y_test).
+    """
+
+    train, test = train_test_split(complete_dataset, test_size=.3, random_state=7, stratify=complete_dataset[TARGET_VARIABLE], shuffle=True)
+
+    return train, test
